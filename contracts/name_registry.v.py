9e97b4f5637f279f232b25a258eca72a724f3104
vishal <vishalreddy616@gmail.com>
2018-09-20 18:17:21 +0530

    added sample sort

--- contracts/name_registry.v.py
+++ contracts/name_registry.v.py
@@ -1,3 +1,7 @@
+#events 
+Payment: event({amount: uint256(wei), arg2: indexed(address)})
+
+
 #address of auctioner
 auctioner: public(address)
 #auction timestamps
@@ -8,11 +12,11 @@ q: public(uint256)
 #number of items
 M: public(uint256)
 #number of notaries registered
-notaries_size: public(uint256)
+notaries_size: public(int128)
 #number of bidders registered
-bidders_size : public(uint256)
+bidders_size : public(int128)
 #Allocating this notary to bidder
-notary_num: public(uint256)
+notary_num: public(int128)
 
 bidders: public({
         notary: address,
@@ -20,21 +24,24 @@ bidders: public({
         isValid : bool
     }[address])
 
-bidder_map : address[uint256]
+bidder_map : address[int128]
 
 notaries : public({
         bidder:address,
         isAssigned : bool,
-        bid_input : uint256[100][1],
-        bid_value : uint256[1][1],
+        bid_input : uint256[100][2],
+        bid_value : uint256[2],
         isValid : bool
     }[address])
 
-notary_map : address[uint256]
+notary_map : address[int128]
+
 
 
 @public
 def __init__(_q: uint256,_M: uint256, _bidding_time: timedelta):
+    assert _M > 0 and _q > 0
+    
     self.auctioner = msg.sender
     self.q = _q
     self.M = _M
@@ -51,16 +58,21 @@ def notaryRegister():
     
     self.notary_map[self.notaries_size] = msg.sender
     self.notaries[msg.sender].isValid = True
-    self.notaries_size = self.notaries_size + 1         
+    self.notaries_size = self.notaries_size + 1  
+
+@public
+@payable
+def __default__():
+    log.Payment(msg.value, msg.sender)
 
 #Next bidders register and assigned a notary based on notary_num value (Not random)
 @public
 @payable
-def bidderRegister(_bid_input:uint256[100][1],_bid_value:uint256[1][1],_num_items:uint256):
+def bidderRegister(_bid_input:uint256[100][2],_bid_value:uint256[2],_num_items:uint256):
     assert self.notary_num < self.notaries_size
     assert not self.bidders[msg.sender].isValid and not self.notaries[msg.sender].isValid
     assert not self.notaries[self.notary_map[self.notary_num]].isAssigned
-    assert _num_items > 0
+    assert _num_items > 0 and _num_items <= self.M
     
     self.bidder_map[self.bidders_size] = msg.sender
     self.bidders_size = self.bidders_size + 1
@@ -74,6 +86,36 @@ def bidderRegister(_bid_input:uint256[100][1],_bid_value:uint256[1][1],_num_item
     self.bidders[msg.sender].num_items = _num_items
     self.notaries[self.bidders[msg.sender].notary].bid_input = _bid_input
     self.notaries[self.bidders[msg.sender].notary].bid_value = _bid_value
- 
 
-    
+@private
+def sqrt(_val:decimal) -> decimal :
+    z : decimal  = (_val+1.0)/2.0
+    y : decimal  = _val
+    for i in range(100):
+        if z < y:
+            break
+        y=z
+        z = (_val/z + z)/2.0
+    return y
+
+@public
+def compare(_a : uint256[2],_b : uint256[2],_n1 : uint256 , _n2 :uint256) -> bool:
+    val1 : decimal = (convert(_a[0],'decimal') - convert(_b[0],'decimal'))/self.sqrt(convert(_n1,'decimal'))
+    val2 : decimal = (convert(_a[1],'decimal') - convert(_b[1],'decimal'))/self.sqrt(convert(_n2,'decimal'))
+    if val1 + val2 < (convert(self.q,'decimal'))/2.0:
+        return True
+    return False
+
+@public
+def winnerDetermine():
+    assert self.auctioner==msg.sender
+    for i in range(100):
+        if i>= self.bidders_size:
+            return
+        for j in range(100):
+            if j >= self.bidders_size-i-1:
+                break
+            if(self.compare(self.notaries[self.bidder_map[j]].bid_value,self.notaries[self.bidder_map[j+1]].bid_value,self.bidders[self.bidder_map[j]].num_items,self.bidders[self.bidder_map[j+1]].num_items)):
+                temp : uint256[2]  = self.notaries[self.bidder_map[j]].bid_value
+                self.notaries[self.bidder_map[j]].bid_value = self.notaries[self.bidder_map[j+1]].bid_value
+                self.notaries[self.bidder_map[j+1]].bid_value = temp
